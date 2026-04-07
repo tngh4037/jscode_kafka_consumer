1fe051632e17d4c278b45bc88a944681a318ccf9
tngh4037 <tngh4037@gmail.com>
2026-04-08 05:39:39 +0900

    feat: kafka 에서 읽어온 메시지 처리 실패시, 재시도 하도록 적용

--- src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
+++ src/main/java/com/example/emailsendconsumer/EmailSendConsumer.java
@@ -1,6 +1,8 @@
 package com.example.emailsendconsumer;
 
 import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.kafka.annotation.RetryableTopic;
+import org.springframework.retry.annotation.Backoff;
 import org.springframework.stereotype.Service;
 
 // kafka 로 부터 메시지를 받아와서 처리하는 Consumer 로직
@@ -11,6 +13,10 @@ public class EmailSendConsumer {
             topics = "email.send",
             groupId = "email-send-group" // 컨슈머 그룹을 활용해서 메시지를 읽기 때문에, offset 을 활용할 수 있다.
     ) // email-send-group 이라는 컨슈머 그룹으로 email.send 토픽의 메시지를 읽어들이겠다. ( email-send-group 이 kafka 에 기존에 생성된 컨슈머 그룹에 없다면, 해당 컨슈머 그룹을 생성해서 메시지를 읽어들인다. 만약 이미 있다면, 기존 생성된 컨슈머 그룹을 활용해서 메시지를 읽는다. )
+    @RetryableTopic(
+            attempts = "5", // 총 시도 가능 횟수 (최초 시도 포함)
+            backoff = @Backoff(delay = 1000, multiplier = 2) // 재시도를 하는 간격 설정 ( 1초 단위로 재시도 하는데, 그 다음번 재시도는 *2배 이후에 시도하겠다. -> ex. 첫 재시도는 1초후, 두번째 재시도는 2초후, 세번째 재시도는 4초후, 네번째 재시도는 8초후, ... )
+    )
     public void consume(String message) {
         System.out.println("Kafka로 부터 받아온 메시지: " + message);
 
@@ -18,12 +24,9 @@ public class EmailSendConsumer {
 
         // 실제 이메일 발송 로직
         // ... 생략 ...
-
-        // ... 실제 이메일 발송 로직은 생략 ...
-        try {
-            Thread.sleep(3000); // 이메일 발송을 하는 데 3초가 걸린다고 가정
-        } catch (InterruptedException e) {
-            throw new RuntimeException("이메일 발송 실패");
+        if (emailSendMessage.getTo().equals("fail")) {
+            System.out.println("잘못된 이메일 주소로 인해 발송 실패");
+            throw new RuntimeException("잘못된 이메일 주소로 인해 발송 실패");
         }
 
         System.out.println("이메일 발송 완료");
